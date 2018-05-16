5b39163ab07f25a0666944277080daac91c4be49
wenjing.chen <wenjing.chen@yingzt.com>
2018-05-16 10:05:54 +0800

    init MultiCurrencyMoney project

---
+++ MultiCurrencyMoney/MultiCurrencyMoney/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  MultiCurrencyMoney
+//
+//  Created by lanjing on 2018/5/16.
+//  Copyright © 2018 xiaoying. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
