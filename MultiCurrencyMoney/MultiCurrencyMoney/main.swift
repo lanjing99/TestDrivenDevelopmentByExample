892ad025512ed68d15dc1d92eb25e5305fe12e3c
wenjing.chen <wenjing.chen@yingzt.com>
2018-05-16 18:01:26 +0800

    Chapter 2. Degenerate Objects

--- MultiCurrencyMoney/MultiCurrencyMoney/main.swift
+++ MultiCurrencyMoney/MultiCurrencyMoney/main.swift
@@ -10,16 +10,35 @@ import Foundation
 
 class Dolor {
     init(_ amount: Int) {
-//        self.amount = amount
+        self.amount = amount
     }
-    var amount : Int = 10
+    var amount : Int
     
-    func times(_ multifier: Int){
-        
+    func times(_ multifier: Int) -> Dolor{
+        return Dolor(amount * multifier)
     }
     
 }
 
 let fiveDolor = Dolor(5)
-fiveDolor.times(2)
-assert(10 == fiveDolor.amount)
+//chapter 1
+//fiveDolor.times(2)
+//degenerate objects
+//assert(10 == fiveDolor.amount)
+//fiveDolor.times(3)
+//assert(15 == fiveDolor.amount)
+
+//ch2
+var product = fiveDolor.times(2)
+assert(10 == product.amount)
+product = fiveDolor.times(3)
+assert(15 == product.amount)
+
+
+
+
+
+
+
+
+
